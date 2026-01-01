b4eae6c5ed6da557028aad17fa9eb898e00a6488
zed <aezakzed@gmail.com>
2026-01-01 14:55:57 +0800

    Update Explore page to use FloatingCard component

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,15 +1,9 @@
+import FloatingCard from '../../components/FloatingCard';
+
 export default function Explore() {
   return (
     <div className="h-screen w-screen bg-black overflow-hidden flex items-center justify-center">
-      {/* Empty rectangle */}
-      <div 
-        style={{
-          width: '90vw',
-          height: '90vh',
-          border: '1px solid white',
-          borderRadius: '24px'
-        }}
-      />
+      <FloatingCard />
     </div>
   )
 }
