d32118606b924d0f32c3a2e1a08206ce70cf3f5f
zed <aezakzed@gmail.com>
2026-01-01 14:14:56 +0800

    Add 'Pathfinder' text in the middle of the explore page

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -2,15 +2,14 @@ export default function Explore() {
   return (
     <div className="h-screen w-screen bg-black overflow-hidden flex items-center justify-center">
       <div 
-        className="border-white rounded-3xl"
+        className="border-white rounded-3xl flex items-center justify-center"
         style={{
           width: '90vw',
           height: '90vh',
           border: '4px solid white'
         }}
       >
-        {/* Test visibility */}
-        <div className="text-white p-4">Test</div>
+        <h1 className="text-white text-6xl font-bold">Pathfinder</h1>
       </div>
     </div>
   )
