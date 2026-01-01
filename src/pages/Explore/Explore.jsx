d62f2f47711963c14ed57420a0eb1cf1a0348a50
zed <aezakzed@gmail.com>
2026-01-01 15:00:02 +0800

    Position Map icon in gap between screen edge and FloatingCard

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -4,8 +4,8 @@ import FloatingCard from '../../components/FloatingCard';
 export default function Explore() {
   return (
     <div className="h-screen w-screen bg-black overflow-hidden flex items-center justify-center relative">
-      {/* Map icon positioned outside the floating card */}
-      <div className="absolute top-8 left-8 z-20">
+      {/* Map icon in the gap between screen edge and floating card */}
+      <div className="absolute z-20" style={{ top: '2.5vh', left: '2.5vw' }}>
         <Map className="text-white" size={40} strokeWidth={2} />
       </div>
       
