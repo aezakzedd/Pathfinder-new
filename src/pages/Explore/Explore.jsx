1b6d0a1f2dc09cfb50e0d13ea9113eeef03f3cc6
zed <aezakzed@gmail.com>
2026-01-01 15:05:39 +0800

    Fix FloatingCard centering on screen

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -3,14 +3,16 @@ import FloatingCard from '../../components/FloatingCard';
 
 export default function Explore() {
   return (
-    <div className="h-screen w-screen bg-black overflow-hidden flex items-center justify-center relative">
-      {/* Map icon with more space from the floating card */}
+    <div className="h-screen w-screen bg-black overflow-hidden">
+      {/* Map icon */}
       <div className="absolute z-20" style={{ top: '1.2vh', left: '1.2vw' }}>
         <Map color="white" size={28} strokeWidth={2} />
       </div>
       
-      {/* Floating Card */}
-      <FloatingCard />
+      {/* Centered container for FloatingCard */}
+      <div className="w-full h-full flex items-center justify-center">
+        <FloatingCard />
+      </div>
     </div>
   )
 }
