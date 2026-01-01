748fd8650847da4cfc981884de4a568aa1ade094
zed <aezakzed@gmail.com>
2026-01-01 15:04:51 +0800

    Make Map icon smaller

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -6,7 +6,7 @@ export default function Explore() {
     <div className="h-screen w-screen bg-black overflow-hidden flex items-center justify-center relative">
       {/* Map icon with more space from the floating card */}
       <div className="absolute z-20" style={{ top: '1.2vh', left: '1.2vw' }}>
-        <Map color="white" size={40} strokeWidth={2} />
+        <Map color="white" size={28} strokeWidth={2} />
       </div>
       
       {/* Floating Card */}
