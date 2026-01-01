7af253162307703a9c8371cb5f309a2caa88d6e3
zed <aezakzed@gmail.com>
2026-01-01 14:11:30 +0800

    Add white outline/border to rectangle

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,7 +1,7 @@
 export default function Explore() {
   return (
     <div className="h-screen w-screen bg-black overflow-hidden flex items-center justify-center">
-      <div className="w-[90vw] h-[90vh] bg-gray-800 rounded-3xl" />
+      <div className="w-[90vw] h-[90vh] border-4 border-white rounded-3xl" />
     </div>
   )
 }
