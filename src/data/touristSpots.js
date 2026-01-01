d64b003916944d3cb2136d67bcd22b26cdbd1f04
zed <aezakzed@gmail.com>
2026-01-02 06:26:38 +0800

    Fix Binurong Point coordinates

--- src/data/touristSpots.js
+++ src/data/touristSpots.js
@@ -3,7 +3,7 @@ export const touristSpots = [
     id: 'binurong-point',
     name: 'Binurong Point',
     location: 'Viga, Catanduanes',
-    coordinates: [124.3067, 13.8667], // Binurong Point coordinates
+    coordinates: [124.41410535976011, 13.66633383393473], // Corrected coordinates [lng, lat]
     rating: 4.9,
     reviewCount: 127,
     image: '/images/binurong-point.jpg', // You'll need to add actual images
