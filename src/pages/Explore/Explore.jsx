e41bfa7073365d7dc378f39cb1c76c5589cc6b20
zed <aezakzed@gmail.com>
2026-01-01 15:04:12 +0800

    Move Map icon away from FloatingCard for better spacing

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -4,8 +4,8 @@ import FloatingCard from '../../components/FloatingCard';
 export default function Explore() {
   return (
     <div className="h-screen w-screen bg-black overflow-hidden flex items-center justify-center relative">
-      {/* Map icon in the gap between screen edge and floating card */}
-      <div className="absolute z-20" style={{ top: '2.5vh', left: '2.5vw' }}>
+      {/* Map icon with more space from the floating card */}
+      <div className="absolute z-20" style={{ top: '1.2vh', left: '1.2vw' }}>
         <Map color="white" size={40} strokeWidth={2} />
       </div>
       
