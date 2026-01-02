8f9fc9d320aa70edc9663e4917696737f1201494
zed <aezakzed@gmail.com>
2026-01-02 14:58:35 +0800

    Add PlaceDetailsSidebar to component exports

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,4 @@ export { default as ChatBot } from './ChatBot';
 export { default as TravellerInformation } from './TravellerInformation';
 export { default as NetworkStatus } from './NetworkStatus';
 export { default as TouristSpotCard } from './TouristSpotCard';
-export { default as ImageCarousel } from './ImageCarousel';
-export { default as TouristSpotDetails } from './TouristSpotDetails';
+export { default as PlaceDetailsSidebar } from './PlaceDetailsSidebar';
